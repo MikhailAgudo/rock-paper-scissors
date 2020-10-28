d019bd77e0b9d7462bd2dccb4ab9f81a34cf2b07
Seraguith <mikhail.agudo@gmail.com>
2020-10-28 15:01:14 +0800

    Fixed the script tag in rps.html. Removed 'break;' in the switch statement, because the interpreter never reaches that point anyway.

--- rps.js
+++ rps.js
@@ -10,15 +10,11 @@ function computerPlay() {
     switch(rpsChoice) {
         case 1:
             return "Rock";
-            break;
         case 2:
             return "Paper";
-            break;
         case 3:
             return "Scissors";
-            break;
         default:
             return;
-            break;
     }
 }
